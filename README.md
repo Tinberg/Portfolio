0d36c9e92ba1b1b06b750f61aa8cd4b993203a10
Mathias Tinberg <mathias@Mathias-sin-MacBook-Pro.local>
2023-09-21 20:50:04 +0200

    dark/light themes

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # Portfolio
 
-fiks så man kan trykke på knappene på linkene i header.
 lag den responsive
 js: 
-fiks dark mode toggle button
+
 parallax scrolling/animations on scroll som AOS
-form validation 
+
 
--- index.js
+++ index.js
@@ -2,6 +2,29 @@
 const year = new Date().getFullYear();
 document.querySelector('footer p').textContent = `© Copyright ${year}. Made by Mathias Tinberg`;
 
+//dark/light mode
+const themeSwitch = document.getElementById("themeSwitch");
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    
+    if (currentTheme === 'dark') {
+        themeSwitch.checked = true;
+    }
+}
+
+themeSwitch.addEventListener("change", function() {
+    if (themeSwitch.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+});
+
 // nodemailer 
 document.querySelector(".form-container").addEventListener("submit", async function(e) {
     e.preventDefault();
@@ -25,6 +48,4 @@ document.querySelector(".form-container").addEventListener("submit", async funct
         alert("An error occurred while sending your message. Please try again later.");
     }
 });
-
-
 

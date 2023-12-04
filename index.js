93f1c5bb69a823c3129b6034d12fc0e857166504
Mathias Tinberg <mathias@Mathias-sin-MacBook-Pro.local>
2023-12-04 09:49:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
-// copyright
+//-------- copyright --------//
 const year = new Date().getFullYear();
 document.querySelector('#copyRight').textContent = `© Copyright ${year}. Made by Mathias Tinberg`;
 
+//-------- hamburger menu --------//
+document.addEventListener("DOMContentLoaded", function() {
+    const menuIcon = document.getElementById("menuIcon");
+    const menuItems = document.getElementById("menuItems");
+    const menuLinks = document.querySelectorAll('#menuItems li a');
 
-// nodemailer 
-document.querySelector(".form-container").addEventListener("submit", async function(e) {
-    e.preventDefault();
-    
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-    const contactInfo = document.getElementById("contactInfo").value;
-
-    const response = await fetch("/.netlify/functions/sendEmail", {
-        method: "POST",
-        body: JSON.stringify({ name, email, message, contactInfo }),
-        headers: {
-            "Content-Type": "application/json"
-        }
+    menuIcon.addEventListener("click", function() {
+        menuItems.classList.toggle("active");
     });
 
-    if (response.ok) {
-        alert("Your message has been sent successfully!");
-    } else {
-        alert("An error occurred while sending your message. Please try again later.");
-    }
+    menuLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            menuItems.classList.remove('active');
+        });
+    });
 });
 
-
-//dark/light mode
+//--------dark/light mode--------//
 document.addEventListener("DOMContentLoaded", function() {
     
 
@@ -58,21 +49,31 @@ themeSwitch.addEventListener("change", function() {
 });
 
 
-// hamburger menu
-document.addEventListener("DOMContentLoaded", function() {
-    const menuIcon = document.getElementById("menuIcon");
-    const menuItems = document.getElementById("menuItems");
-    const menuLinks = document.querySelectorAll('#menuItems li a');
-
-    menuIcon.addEventListener("click", function() {
-        menuItems.classList.toggle("active");
-    });
-
-    menuLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            menuItems.classList.remove('active');
-        });
-    });
-});
+//--------Form --------//
 
 //Form validation 
+
+
+// nodemailer 
+document.querySelector(".form-container").addEventListener("submit", async function(e) {
+    e.preventDefault();
+    
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    const contactInfo = document.getElementById("contactInfo").value;
+
+    const response = await fetch("/.netlify/functions/sendEmail", {
+        method: "POST",
+        body: JSON.stringify({ name, email, message, contactInfo }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    if (response.ok) {
+        alert("Your message has been sent successfully!");
+    } else {
+        alert("An error occurred while sending your message. Please try again later.");
+    }
+});

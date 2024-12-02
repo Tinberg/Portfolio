1920195d55d992dece98ec41d78b55abb87c68b8
mathias <tinberg92@hotmail.com>
2024-12-02 21:54:39 +0100

    Update sendEmail.js

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -1,57 +1,18 @@
-// const nodemailer = require("nodemailer");
-
-// exports.handler = async function (event, context) {
-//   if (event.httpMethod !== "POST") {
-//     return { statusCode: 405, body: "Method Not Allowed" };
-//   }
-
-//   const { name, email, message, contactInfo } = JSON.parse(event.body);
-//   const senderEmail = "tinberg92@hotmail.com";
-
-//   const transporter = nodemailer.createTransport({
-//     service: "hotmail",
-//     auth: {
-//       user: process.env.EMAIL_USER,
-//       pass: process.env.EMAIL_PASS,
-//     },
-//   });
-
-//   const mailOptions = {
-//     from: senderEmail,
-//     to: senderEmail,
-//     subject: `Message from ${name}`,
-//     text: `message: ${message}\nEmail from: ${email}`,
-//   };
-
-//   try {
-//     await transporter.sendMail(mailOptions);
-//     return { statusCode: 200, body: "Email sent!" };
-//   } catch (error) {
-//     return { statusCode: 500, body: "Error sending email: " + error.message };
-//   }
-// };
-
-
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 exports.handler = async function (event, context) {
-  if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method Not Allowed' };
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
   }
 
   const { name, email, message, contactInfo } = JSON.parse(event.body);
-  const senderEmail = 'tinberg92@hotmail.com';
+  const senderEmail = "tinberg92@hotmail.com";
 
   const transporter = nodemailer.createTransport({
-    host: 'smtp.office365.com', // Use the correct SMTP server
-    port: 587, // Port for TLS
-    secure: false, // Upgrade later with STARTTLS
+    service: "hotmail",
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
-    },
-    tls: {
-      ciphers: 'SSLv3',
     },
   });
 
@@ -59,18 +20,13 @@ exports.handler = async function (event, context) {
     from: senderEmail,
     to: senderEmail,
     subject: `Message from ${name}`,
-    text: `Message: ${message}\nEmail from: ${email}`,
+    text: `message: ${message}\nEmail from: ${email}`,
   };
 
   try {
-    const info = await transporter.sendMail(mailOptions);
-    console.log('Email sent:', info);
-    return { statusCode: 200, body: 'Email sent!' };
+    await transporter.sendMail(mailOptions);
+    return { statusCode: 200, body: "Email sent!" };
   } catch (error) {
-    console.error('Error sending email:', error);
-    return {
-      statusCode: 500,
-      body: `Error sending email: ${error.message}`,
-    };
+    return { statusCode: 500, body: "Error sending email: " + error.message };
   }
 };

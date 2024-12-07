4e45041f9c7d3162a5de6569ba2c4a3c3331477e
mathias <tinberg92@hotmail.com>
2024-12-07 12:48:59 +0100

    nodemailer fixed

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -1,32 +1,35 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
-exports.handler = async function (event, context) {
-  if (event.httpMethod !== "POST") {
-    return { statusCode: 405, body: "Method Not Allowed" };
-  }
+exports.handler = async function(event, context) {
+    
+    if (event.httpMethod !== "POST") {
+        return { statusCode: 405, body: "Method Not Allowed" };
+    }
 
-  const { name, email, message, contactInfo } = JSON.parse(event.body);
-  const senderEmail = "tinberg92@hotmail.com";
+    const { name, email, message, contactInfo } = JSON.parse(event.body);
+    const senderEmail = 'tinberg92@hotmail.com';
 
-  const transporter = nodemailer.createTransport({
-    service: "hotmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+    const transporter = nodemailer.createTransport({
+        service: 'hotmail', 
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
 
-  const mailOptions = {
-    from: senderEmail,
-    to: senderEmail,
-    subject: `Message from ${name}`,
-    text: `message: ${message}\nEmail from: ${email}`,
-  };
+    const mailOptions = {
+        from: senderEmail,
+        to: senderEmail, 
+        subject: `Message from ${name}`,
+        text: `message: ${message}\nEmail from: ${email}` 
+        
+    };
 
-  try {
-    await transporter.sendMail(mailOptions);
-    return { statusCode: 200, body: "Email sent!" };
-  } catch (error) {
-    return { statusCode: 500, body: "Error sending email: " + error.message };
-  }
+    try {
+        await transporter.sendMail(mailOptions);
+        return { statusCode: 200, body: "Email sent!" };
+    } catch (error) {
+        return { statusCode: 500, body: "Error sending email: " + error.message };
+    }
 };
+

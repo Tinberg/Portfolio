2e93fad99eb779e88f3d849c9aa14c83c9c89a2f
mathias <tinberg92@hotmail.com>
2024-12-07 13:17:00 +0100

    Update sendEmail.js

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -1,12 +1,16 @@
 const nodemailer = require('nodemailer');
 
 exports.handler = async function(event, context) {
-    
+    console.log('Function triggered');
+
     if (event.httpMethod !== "POST") {
+        console.log('Method Not Allowed');
         return { statusCode: 405, body: "Method Not Allowed" };
     }
 
     const { name, email, message, contactInfo } = JSON.parse(event.body);
+    console.log(`Received message from: ${name}, email: ${email}`);
+
     const senderEmail = 'tinberg92@hotmail.com';
 
     const transporter = nodemailer.createTransport({
@@ -15,24 +19,24 @@ exports.handler = async function(event, context) {
         secure: false, // use STARTTLS
         auth: {
             user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
+            pass: process.env.EMAIL_PASS
         }
     });
-    
 
     const mailOptions = {
         from: senderEmail,
-        to: senderEmail, 
+        to: senderEmail,
         subject: `Message from ${name}`,
-        text: `message: ${message}\nEmail from: ${email}` 
-        
+        text: `Message: ${message}\nEmail from: ${email}`
     };
 
     try {
+        console.log('Sending email...');
         await transporter.sendMail(mailOptions);
+        console.log('Email sent successfully!');
         return { statusCode: 200, body: "Email sent!" };
     } catch (error) {
+        console.error('Error sending email:', error);
         return { statusCode: 500, body: "Error sending email: " + error.message };
     }
 };
-

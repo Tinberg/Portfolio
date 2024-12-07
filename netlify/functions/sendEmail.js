b5bc530cd20a1799edeefbac221c6f5cb40a1854
mathias <tinberg92@hotmail.com>
2024-12-07 12:56:01 +0100

    Update sendEmail.js

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -10,10 +10,12 @@ exports.handler = async function(event, context) {
     const senderEmail = 'tinberg92@hotmail.com';
 
     const transporter = nodemailer.createTransport({
-        service: 'hotmail', 
+        host: 'smtp-mail.outlook.com',  
+        port: 587,                    
+        secure: false,                   
         auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
+            user: process.env.EMAIL_USER,  
+            pass: process.env.EMAIL_PASS   
         }
     });
 
